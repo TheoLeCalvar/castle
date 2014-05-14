19d2743022a24f73f7b7ee454e3037f6649ffe5b
Théo Le Calvar <goldensuneur@gmail.com>
2014-05-14 16:26:46 +0200

    Mise à jour des ressources

--- objets/node.hpp
+++ objets/node.hpp
@@ -1,7 +1,7 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
-#include "Mesh.hpp"
+#include "mesh.hpp"
 #include <assimp/Importer.hpp> // C++ importer interface
 #include <assimp/scene.h> // Output data structure
 #include <assimp/postprocess.h> // Post processing flags
@@ -13,19 +13,39 @@
 #include <QMultiMap>
 #include <QPair>
 
+/**
+ * @class Node
+ * @brief Contient un node de modèle 3D
+ * @details Un node possède un ensemble de meshes et de Node fils
+ * 
+ */
 class Node: public Objet
 {
 private:
 
-	QList<Mesh *> _meshs;
-	QMap<QString, Node *> _children;
+	QList<Mesh *> _meshs; /**< Liste des Mesh de ce node **/
+	QMap<QString, Node *> _children; /**< Fils du Node **/
 
-	static QMultiMap<QString, Node *> _loadedModels;
+	static QMultiMap<QString, Node *> _loadedModels; /**< Modèles déjà chargés, recnstruit un ensemble de Node par recopie **/
 
+	/**
+	 * @brief Constructeur par défaut
+	 */
 	Node();
+
+	/**
+	 * @brief Constructeur par recopie
+	 * 
+	 * @param node Node à recopier
+	 */
 	Node(const Node & node);
 
-
+	/**
+	 * @brief Charge un Material depuis une aiMaterial d'ASSIMP 
+	 * 
+	 * @param mtl material à charger
+	 * @return Material créé
+	 */
 	static Material * 	loadMaterial(const aiMaterial * mtl);
 	static Node * 		loadNode(const aiNode * node, const aiScene * pScene, Scene * scene);
 
