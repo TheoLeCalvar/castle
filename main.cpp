c68bc580881b3fd849fd86f019cce310c82d1c6e
baptiste <etudiant@26S.localdomain>
2014-05-23 12:08:32 +0200

    raccoursis + new /open/save (suite)

--- main.cpp
+++ main.cpp
@@ -6,17 +6,23 @@
 
 #include <QString>
 #include <QDebug>
+#include <QtGui>
 
-
+#ifdef Q_WS_MAC
+extern void qt_set_sequence_auto_mnemonic(bool b);
+#endif
 
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);	
 	QDir dir(QApplication::applicationDirPath());
 
+
 	#ifdef Q_OS_OSX
 		dir.cdUp();
 		dir.cd("Resources");
+        qt_se
+        qt_set_sequence_auto_mnemonic(true);
 	#endif
 
 	QDir::setCurrent(dir.absolutePath());
