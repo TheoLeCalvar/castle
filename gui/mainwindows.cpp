7fedae6915f2bcf42cf3fb407a23a0b1b5c5a7d6
baptiste <etudiant@26S.localdomain>
2014-05-24 10:44:47 +0200

    supression presque ok

--- gui/mainwindows.cpp
+++ gui/mainwindows.cpp
@@ -1030,7 +1030,10 @@ void MainWindow::affichagerecnoderestant(Node *a ,QStandardItem *b )
         if( modele->data ( (vue->selectionModel()->currentIndex().parent().parent()), Qt::DisplayRole)=="Objet")
             {
             widget->getScene()->getPiece(vue->selectionModel()->currentIndex().parent().data().toString())->removeChild(vue->selectionModel()->currentIndex().data().toString());
-            item->removeRow(vue->selectionModel()->currentIndex().row());
+
+            objet->child(vue->selectionModel()->currentIndex().parent().row())->removeRow(vue->selectionModel()->currentIndex().row());
+                        std::cout<< vue->selectionModel()->currentIndex().parent().data().toString().toStdString() << std::endl;
+                        std::cout<<vue->selectionModel()->currentIndex().data().toString().toStdString() <<std::endl;
             }
     }
 
